c6928574f37cfb0f602d44714cdd2f06a82326ff
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-12-16 15:02:28 +0200

    done with second test case

--- src/test/java/rw/ac/rca/termOneExam/controller/CityControllerIntegrationTest.java
+++ src/test/java/rw/ac/rca/termOneExam/controller/CityControllerIntegrationTest.java
@@ -16,6 +16,7 @@ import rw.ac.rca.termOneExam.service.CityService;
 
 import java.util.Arrays;
 
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,10 +28,26 @@ public class CityControllerIntegrationTest {
     private MockMvc mockMvc;
     @MockBean
     private CityService cityService;
+
     @Test
-    public void getAll_test() throws Exception{
-     when(cityService.getAll()).thenReturn(Arrays.asList(new City(1L,"Kigali",24,23),new City(2L,"Musanze",18,23),new City(3L,"Nyabihu",16,23)));
+    public void getAll_test() throws Exception {
+        when(cityService.getAll()).thenReturn(Arrays.asList(new City(1L, "Kigali", 24, 23), new City(2L, "Musanze", 18, 23), new City(3L, "Nyabihu", 16, 23)));
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/api/cities/all").accept(MediaType.APPLICATION_JSON);
         mockMvc.perform(request).andExpect(status().isOk()).andReturn();
     }
+
+    @Test
+    public void findById_test() throws Exception {
+        when(cityService.getById(anyLong())).thenReturn(java.util.Optional.of(new City(101, "Kigali",23,0)));
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/api/cities/101").accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request).andExpect(status().isOk()).andReturn();
+    }
+//    @Test
+//    public void findById_NotFound_test() throws Exception{
+//        when(cityService.getById(anyLong())).thenThrow(new Exception("Not found"), HttpStatus.NOT_FOUND));
+//
+//        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/api/cities/1");
+//
+//        mockMvc.perform(request).andExpect(status().isNotFound()).andExpect(content().string("Contact with this id not found")).andReturn();
+//    }
 }
